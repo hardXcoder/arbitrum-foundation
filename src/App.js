c1ab5e12a8f7dbd02f8c08b28204c54eeebdcee7
ujjwal <ujjwal@intract.buzz>
2023-03-23 21:01:27 +0530

    First draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import oops from './oops';
+import { CircleLoader } from 'react-spinners';
+
+
+
 
 function App() {
+
+  const [text, setText] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+  function handleInputChange(event) {
+    setText(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setLoading(true);
+
+    const response = await fetch('https://twitterapi.ftwdao.xyz/api/check-address', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
+      body: JSON.stringify({
+        "address": text,
+        "url": "sdf"
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    // setTimeout(() => {
+    //   setLoading(true);
+    // }, 5000);
+
+    setLoading(false);
+
+    setSubmitted(true);
+
+    // navigate('/oops');
+
+    // history.push('/oops');
+
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+
+        <p
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          <b>Check your hidden arbitrum airdrop ? </b>
+        </p>
+
+
+
+        {loading ? (
+          <div className="loader-container">
+            <CircleLoader color="#669ACF" size={100} />
+          </div>
+        ) : submitted ? (
+          <h2 className="App-link">Oops! You got no extra airdrop.</h2>
+        ) :
+          (
+            <div class="form-container">
+              <form onSubmit={handleSubmit}>
+                <div class="input-container">
+                  <input type="text" placeholder="Just enter your arbitrum address here..." value={text} onChange={handleInputChange} />
+                </div>
+                <button type="submit" class="submit-button">Submit</button>
+              </form>
+            </div>
+          )
+        }
+
+
+
+
+
       </header>
     </div>
   );
---
+++ src/oops.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="App-link">Oops! You got no hidden/extra airdrop.</h2>
+    </div >
+  );
+}
+
+export default App;
